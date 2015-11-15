9d8b9fcf87f882bb95705978342e27678253c0c6
fentas <jan.guth@gmail.com>
2015-11-15 01:51:16 +0100

    keep newlines

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,10 +4,10 @@ envtpl /usr/src/app/thumbor.conf.tpl  --allow-missing --keep-template
 
 if [ -z $1 ] || [ ${1:0:1} = '-' ]; then
   if [ ! -z "${NGINX_RESTRICT}" ]; then
-    echo ${NGINX_RESTRICT} > /etc/nginx/includes/nginx-restrict
+    printf "${NGINX_RESTRICT}" > /etc/nginx/includes/nginx-restrict
   fi
   if [ ! -z "${NGINX_HTPASSWD}" ]; then
-    echo ${NGINX_HTPASSWD} > /etc/nginx/htpasswd
+    printf "${NGINX_HTPASSWD}" > /etc/nginx/htpasswd
   fi
   thumbor --port=80 --conf=/usr/src/app/thumbor.conf &
   set -- nginx -g 'daemon off;' "$@"
